91d7d091509b240c3edd46901ab3e56c37ecb193
Diego Morales <98107176+diegomorales30@users.noreply.github.com>
2022-05-08 07:36:37 -0600

    errases notes

--- Circles/src/edu/nmsu/cs/circles/Circle2Test.java
+++ Circles/src/edu/nmsu/cs/circles/Circle2Test.java
@@ -1,14 +1,5 @@
 package edu.nmsu.cs.circles;
 
-/***
- * Example JUnit testing class for Circle2 (and Circle)
- *
- * - must have your classpath set to include the JUnit jarfiles - to run the test do: java
- * org.junit.runner.JUnitCore Circle2Test - note that the commented out main is another way to run
- * tests - note that normally you would not have print statements in a JUnit testing class; they are
- * here just so you see what is happening. You should not have them in your test cases.
- ***/
-
 import org.junit.*;
 
 public class Circle2Test
@@ -35,7 +26,15 @@ public class Circle2Test
 		System.out.println("\nTest finished.");
 	}
 
-   
+
+    @Test
+    public void testScaleGrowth(){
+        System.out.println("Test for scale of a big circle");
+        double factorForScale = 2.5;
+        double orginalRadius = circle2.radius;
+        double newRadius = circle2.scale(factorForScale);
+        Assert.assertTrue(newRadius == orginalRadius*factorForScale);
+    }
 
 	//
 	// Test a simple positive move
